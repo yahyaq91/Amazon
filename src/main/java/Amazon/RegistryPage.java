239f88ce5a6a9e3777468928095cf0797d82270f
yahyaq91 <yahyaq91@gmail.com>
2022-04-17 20:13:16 -0400

    started on test cases

--- src/main/java/Amazon/RegistryPage.java
+++ src/main/java/Amazon/RegistryPage.java
@@ -1,6 +1,8 @@
 package Amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -8,9 +10,32 @@ public class RegistryPage extends BasePage {
 
     private static final String URL = "https://www.amazon.com/";
 
+    @FindBy(xpath= "//select[@name='searchUrl']")
+    public WebElement dropdown;
+
+    @FindBy(id = "dropdown1_0")
+    public WebElement dropdownWeddingRegistry;
+
+    @FindBy(id = "dropdown1_1")
+    public WebElement dropdownBabyRegistry;
+
+    @FindBy(id = "dropdown1_2")
+    public WebElement dropdownBirthdayGiftList;
+
+    @FindBy(id = "dropdown1_3")
+    public WebElement dropdownCustomGiftList;
+
+    @FindBy(id = "dropdown1_4")
+    public WebElement dropdownHolidayGiftList;
+
+
     public RegistryPage() {
         webDriverWait.until(ExpectedConditions.urlContains(URL));
         PageFactory.initElements(driver, this);
+    }
+
+    public void selectDropdownOption(String visibleText) {
+        selectFromDropdownByValue(dropdown, visibleText);
     }
 
 }
--- src/test/java/Amazon/TestRegistryPage.java
+++ src/test/java/Amazon/TestRegistryPage.java
@@ -1,6 +1,21 @@
 package Amazon;
 
 import base.TestBasePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestRegistryPage extends TestBasePage {
+    @Test
+    public void testSelectFromDropdown() {
+        Homepage homepage = new Homepage();
+        RegistryPage registryPage = homepage.navigateToRegistryPage();
+        registryPage.selectDropdownOption("Wedding Registry");
+
+        Assert.assertTrue(registryPage.dropdownWeddingRegistry.isSelected());
+    }
+
+
+
+
+
 }
