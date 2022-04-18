6e91bb726e0043396b3562b1c0a2bf95af374bc8
yahyaq91 <yahyaq91@gmail.com>
2022-04-17 22:18:31 -0400

    5 Test cases writen and tested for RegisrtyPage

--- src/main/java/Amazon/RegistryPage.java
+++ src/main/java/Amazon/RegistryPage.java
@@ -10,23 +10,45 @@ public class RegistryPage extends BasePage {
 
     private static final String URL = "https://www.amazon.com/";
 
-    @FindBy(xpath= "//select[@name='searchUrl']")
+    @FindBy(xpath = "//select[@name='searchUrl']")
     public WebElement dropdown;
 
-    @FindBy(id = "dropdown1_0")
-    public WebElement dropdownWeddingRegistry;
+//    @FindBy(xpath = "//option[@value='/wedding/search?nameOrEmail=']")
+//    public WebElement dropdownWeddingRegistry;
+//
+//    @FindBy(xpath = "//option[@value='/baby-reg/search-results?nameOrEmail=']")
+//    public WebElement dropdownBabyRegistry;
+//
+//    @FindBy(xpath = "//option[@value='/registries/birthday/search?name=']")
+//    public WebElement dropdownBirthdayGiftList;
+//
+//    @FindBy(xpath = "//option[@value='/registries/custom/search?name=']")
+//    public WebElement dropdownCustomGiftList;
+//
+//    @FindBy(xpath = "//option[@value='/registries/holiday/search?name=']")
+//    public WebElement dropdownHolidayGiftList;
+    // endregion
 
-    @FindBy(id = "dropdown1_1")
-    public WebElement dropdownBabyRegistry;
+    @FindBy(xpath =("//button[@class='gr-btn gr-btn--rounded gr-text gr-find-stripe__cta gr-find-stripe__submit']"))
+    public WebElement searchButton;
 
-    @FindBy(id = "dropdown1_2")
-    public WebElement dropdownBirthdayGiftList;
+    @FindBy(name = "name")
+    public WebElement searchBar;
 
-    @FindBy(id = "dropdown1_3")
-    public WebElement dropdownCustomGiftList;
+    @FindBy(xpath = "//*[@id=\"wr_search_result_record_0-owner\"]/div/a")
+    public WebElement weddingRegistryFirstResult;
 
-    @FindBy(id = "dropdown1_4")
-    public WebElement dropdownHolidayGiftList;
+    @FindBy(xpath = "//*[@id=\"search-results-box\"]//div[3]//tbody/tr[2]")
+    public WebElement babyRegistryFirstResult;
+
+    @FindBy(xpath = "//*[@id=\"search-result-container\"]/li[2]/div[1]/a")
+    public WebElement birthdayGiftListFirstResult;
+
+    @FindBy(xpath = "//*[@id=\"search-result-container\"]/li[2]/div[1]/a")
+    public WebElement customGiftListFirstResult;
+
+    @FindBy(xpath = "//*[@id=\"search-result-container\"]/li[2]/div[1]/a")
+    public WebElement holidayRegistryFirstResult;
 
 
     public RegistryPage() {
--- src/test/java/Amazon/TestRegistryPage.java
+++ src/test/java/Amazon/TestRegistryPage.java
@@ -6,16 +6,49 @@ import org.testng.annotations.Test;
 
 public class TestRegistryPage extends TestBasePage {
     @Test
-    public void testSelectFromDropdown() {
+    public void testWeddingRegistryDropdownOption() {
         Homepage homepage = new Homepage();
         RegistryPage registryPage = homepage.navigateToRegistryPage();
-        registryPage.selectDropdownOption("Wedding Registry");
-
-        Assert.assertTrue(registryPage.dropdownWeddingRegistry.isSelected());
+        selectFromDropdownByIndex(registryPage.dropdown, 1);
+        registryPage.searchBar.sendKeys("Alan");
+        registryPage.searchButton.click();
+        registryPage.weddingRegistryFirstResult.click();
     }
-
-
-
-
+    @Test
+    public void testBabyRegistryDropdownOption() {
+        Homepage homepage = new Homepage();
+        RegistryPage registryPage = homepage.navigateToRegistryPage();
+        selectFromDropdownByIndex(registryPage.dropdown, 2);
+        registryPage.searchBar.sendKeys("Bob");
+        registryPage.searchButton.click();
+        registryPage.babyRegistryFirstResult.click();
+    }
+    @Test
+    public void testBirthdayGiftListDropdownOption() {
+        Homepage homepage = new Homepage();
+        RegistryPage registryPage = homepage.navigateToRegistryPage();
+        selectFromDropdownByIndex(registryPage.dropdown, 3);
+        registryPage.searchBar.sendKeys("Jack");
+        registryPage.searchButton.click();
+        registryPage.birthdayGiftListFirstResult.click();
+    }
+    @Test
+    public void testCustomGiftListDropdownOption() {
+        Homepage homepage = new Homepage();
+        RegistryPage registryPage = homepage.navigateToRegistryPage();
+        selectFromDropdownByIndex(registryPage.dropdown, 4);
+        registryPage.searchBar.sendKeys("John");
+        registryPage.searchButton.click();
+        registryPage.customGiftListFirstResult.click();
+    }
+    @Test
+    public void testHolidayGiftListDropdownOption() {
+        Homepage homepage = new Homepage();
+        RegistryPage registryPage = homepage.navigateToRegistryPage();
+        selectFromDropdownByIndex(registryPage.dropdown, 5);
+        registryPage.searchBar.sendKeys("Peter");
+        registryPage.searchButton.click();
+        registryPage.holidayRegistryFirstResult.click();
+    }
 
 }
