86ee1ea17d98e3bdb888e56e09cb0513b8757e1e
abbagaro <abbagaroj@gmail.com>
2022-04-21 11:22:08 -0400

    5 Test cases created and tested with assertions

--- src/main/java/Amazon/GiftCardPage.java
+++ src/main/java/Amazon/GiftCardPage.java
@@ -1,7 +1,33 @@
 package Amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class GiftCardPage extends BasePage {
 
+    private static final String URL = "https://www.amazon.com";
+
+    @FindBy(xpath = "//*[@id=\"s-refinements\"]/div[1]/ul/li[1]/span/a/div/label/i" )
+    public WebElement eGiftCard;
+
+    @FindBy(xpath = "//body/div[@id='a-page']/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[1]/img[1]")
+    public WebElement thankYouCard;
+
+    @FindBy(xpath = "//body/div[@id='a-page']/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[5]/div[1]/div[1]/a[1]/img[1]")
+    public  WebElement weddingCard;
+
+    @FindBy(xpath = "//body/div[@id='a-page']/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/a[1]/img[1]")
+    public WebElement birthdayCard;
+
+    @FindBy(xpath = "//body/div[@id='a-page']/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[6]/div[1]/div[1]/a[1]/img[1]")
+    public WebElement congratsCard;
+
+
+    public GiftCardPage() {
+        webDriverWait.until(ExpectedConditions.urlContains(URL));
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/test/java/Amazon/TestGiftCardPage.java
+++ src/test/java/Amazon/TestGiftCardPage.java
@@ -1,7 +1,68 @@
 package Amazon;
 
 import base.TestBasePage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestGiftCardPage extends TestBasePage {
+    @Test
+    public void testNavigationToEGiftCard(){
+        Homepage homepage = new Homepage();
+        GiftCardPage giftCardPage = homepage.navigateToGiftCardPage();
+        giftCardPage.eGiftCard.click();
+
+        String expectedText = "RESULTS";
+        String text = driver.findElement(By.xpath("//div[@tabindex='0']")).getText();
+        System.out.println(text);
+        Assert.assertEquals(expectedText, text);
+
+    }
+    @Test
+    public void testNavigationToThankYouCard(){
+        Homepage homepage = new Homepage();
+        GiftCardPage giftCardPage = homepage.navigateToGiftCardPage();
+        giftCardPage.thankYouCard.click();
+
+        String expectedText = "Amazon.com: Thank You and Appreciation Gift Cards: Gift Cards";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText, text);
+
+    }
+    @Test
+    public void testNavigationToWeddingCard(){
+        Homepage homepage = new Homepage();
+        GiftCardPage giftCardPage = homepage.navigateToGiftCardPage();
+        giftCardPage.weddingCard.click();
+
+        String expectedText = "Amazon.com: Wedding & Engagement: Gift Cards";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText, text);
+    }
+    @Test
+    public void testNavigationToBirthdayCard(){
+        Homepage homepage = new Homepage();
+        GiftCardPage giftCardPage = homepage.navigateToGiftCardPage();
+        giftCardPage.birthdayCard.click();
+
+        String expectedText = "Amazon.com Birthday Gift Cards";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText, text);
+    }
+    @Test
+    public void testNavigationToCongratsCard(){
+        Homepage homepage = new Homepage();
+        GiftCardPage giftCardPage = homepage.navigateToGiftCardPage();
+        giftCardPage.congratsCard.click();
+
+        String expectedText = "Amazon.com: Congratulations - Occasions: Gift Cards";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText, text);
+    }
+
 
 }
